3fa015b6892e8ca6ded56560fbacbf27deb193de
Denizinsek <denizinansimsek@outlook.com>
2024-06-23 02:59:52 +0300

    products controller

---
+++ DataExtractionLogin/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using DataExtractionLogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataExtractionLogin.Controllers
+{
+    public class ProductsController : Controller
+    {
+        MvcModelFirstEntities db = new MvcModelFirstEntities();
+        // GET: Products
+
+        // Product List
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        // Product Save
+        public ActionResult Save()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Save(Products products)
+        {
+            db.Products.Add(products);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
