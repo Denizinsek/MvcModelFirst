28719ba262d52bb574a5a438abbdd8aefd52aedd
Denizinsek <denizinansimsek@outlook.com>
2024-06-23 03:17:52 +0300

    home controller

---
+++ DataExtractionLogin/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using DataExtractionLogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataExtractionLogin.Controllers
+{
+    public class HomeController : Controller
+    {
+        MvcModelFirstEntities db = new MvcModelFirstEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+    }
+}
